f788caf96a1fe3b59f26332cb90ed02f24b4e0b9
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-18 13:44:21 -0800

    Created a queue using functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,28 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // Keep track of newest and oldest indexes
+  let oIndex = 1;
+  let nIndex = 1;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[nIndex] = value;
+    nIndex++;
   };
 
   someInstance.dequeue = function() {
+    if (oIndex !== nIndex) {
+      mem = storage[oIndex];
+      delete storage[oIndex];
+      oIndex++;
+      return mem;
+    }
   };
 
   someInstance.size = function() {
+    return nIndex - oIndex;
   };
 
   return someInstance;
