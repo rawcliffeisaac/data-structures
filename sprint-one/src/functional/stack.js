c4cd383e95777ce7e1d71d08c1c7250bd63ed53d
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-18 13:17:10 -0800

    Created a stack using functional instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,26 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // Keep track of the size of the stack
+  var size = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      size--;
+      let mem = storage[size];
+      delete storage[size];
+      return mem;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
