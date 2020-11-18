5a9725fd26a747f2f5531b0e0461f72008f05cc9
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-18 15:54:05 -0800

    Created a queue using functional instantiation with shared methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someinstance = {
+    oIndex: 1,
+    nIndex: 1,
+    storage: {}
+  };
+  _.extend(someinstance, queueMethods);
+  return someinstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.nIndex] = value;
+    this.nIndex++;
 
+  },
+  dequeue: function() {
+    if (this.oIndex !== this.nIndex) {
+      var mem = this.storage[this.oIndex];
 
+      delete this.storage[this.oIndex];
+      this.oIndex++;
+      return mem;
+    }
+  },
+  size: function() {
+    return this.nIndex - this.oIndex;
+  }
+};
