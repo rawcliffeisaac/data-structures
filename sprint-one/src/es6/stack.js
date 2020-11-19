720424c560d40a82c829b4bf1fd5bdb163daab3b
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-19 10:27:27 -0800

    Created a stack using ES6 instantiation

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.newsize = 0;
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.newsize] = value;
+    this.newsize++;
+  }
+
+  pop() {
+    if (this.newsize > 0) {
+      this.newsize--;
+      let mem = this.storage[this.newsize];
+      delete this.storage[this.newsize];
+      return mem;
+    }
+  }
+
+  size() {
+    return this.newsize;
+  }
 }
