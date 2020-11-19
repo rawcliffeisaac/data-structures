7b4b7c861d318a01505bf32909eddcecf426a1dc
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-19 08:20:06 -0800

    Created a queue using prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.nIndex = 1;
+  someInstance.oIndex = 1;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.nIndex] = value;
+    this.nIndex++;
 
+  },
+  dequeue: function() {
+    if (this.oIndex !== this.nIndex) {
+      var mem = this.storage[this.oIndex];
 
+      delete this.storage[this.oIndex];
+      this.oIndex++;
+      return mem;
+    }
+  },
+  size: function() {
+    return this.nIndex - this.oIndex;
+  }
+};
