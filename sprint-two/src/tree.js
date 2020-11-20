b88df5c470b385cdbf23b4641a8523df98bd57ff
rawcliffeisaac <rawcliffe.isaac@gmail.com>
2020-11-19 18:34:30 -0800

    Implements tree functionality (addChild, contains)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,29 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (this.children.length > 0) {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
